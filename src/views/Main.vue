7f5bbbe95519f1219a5f01d27fe7012f10f09ec1
jaeyoung3048 <wanna33008@gmail.com>
2021-08-02 16:53:53 +0900

    feat: Added views and router
    각 페이지와 라우터 추가

---
+++ src/views/Main.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="main">
+    <Header />
+  </div>
+</template>
+
+<script>
+import Header from '../components/Header.component.vue'
+
+export default {
+  name: 'Main',
+  components: {
+    Header,
+  },
+}
+</script>
