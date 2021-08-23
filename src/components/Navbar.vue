ed876460f4bb67ac30e55582a364420cbb881dc4
jaeyoung3048 <wanna33008@gmail.com>
2021-08-24 01:27:29 +0900

    feat: 카테고리 탭 생성
    v-for를 통해 카테고리 탭의 요소들을 생성

---
+++ src/components/Navbar.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="category-navigation-container">
+    <div class="category-navigation-navigation-bar">
+      <button class="category-navigation-tab-button all-category">
+        전체 카테고리
+        <img src="../assets/dropdown_icon.svg" />
+      </button>
+      <button class="category-navigation-tab-button event">7월 가입혜택</button>
+      <button
+        v-for="data in navigationData"
+        :key="data"
+        class="category-navigation-tab-button"
+      >
+        {{ data }}
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data() {
+    return {
+      navigationData: ['바로수강', '얼리버드', '오픈 예정', '이벤트'],
+      dummyData: {
+        드로잉: ['펜, 연필', '마카', '색연필'],
+        공예: ['실 공예', '대바늘뜨개', '실바늘뜨개'],
+        '요리, 음료': ['아시안', '양식', '중식'],
+      },
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+.category-navigation-container {
+  position: relative;
+  width: 100%;
+  background-color: rgb(255, 255, 255);
+  .category-navigation-navigation-bar {
+    position: relative;
+    width: 100%;
+    max-width: 1176px;
+    margin: 0px auto;
+    background-color: rgb(255, 255, 255);
+    display: flex;
+    align-items: center;
+    overflow: auto;
+    justify-content: flex-start;
+    .category-navigation-tab-button {
+      font-size: 16px;
+      line-height: 24px;
+      margin-right: 28px;
+      padding: 8px 0px 20px;
+      display: flex;
+      box-sizing: border-box;
+      border: none;
+      outline: none;
+      background-color: white;
+      color: rgb(26, 26, 26);
+      font-weight: bold;
+      cursor: pointer;
+    }
+    .event {
+      color: rgb(29, 78, 250);
+    }
+  }
+}
+</style>
