3258034977875669a2670fa2c3a10f37029cb44e
jaeyoung3048 <wanna33008@gmail.com>
2021-09-06 09:19:32 +0900

    feat: 메인 페이지에 배너 추가

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="main">
-    <main-header />
+    <Header />
+    <Banner />
   </div>
 </template>
 
 <script>
 import Header from '../components/Header.vue'
+import Banner from '../components/Banner.vue'
 
 export default {
   name: 'Main',
   components: {
-    'main-header': Header,
+    Header: Header,
+    Banner: Banner,
   },
 }
 </script>
